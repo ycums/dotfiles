0dceb005e803a50fc3a1793e783721de7f328020
ycums <ycums00@gmail.com>
2019-12-27 09:14:43 +0000

    シェル変数周りを整理

--- .zsh/rc/70_misc.zsh
+++ .zsh/rc/70_misc.zsh
@@ -1,10 +1,11 @@
 WORDCHARS='*?_-.[]~=&;!#$%^(){}<>'
+
+HISTFILE=${HOME}/.zsh_history
+HISTSIZE=10000
+SAVEHIST=10000
+
 if [ "$(uname)" = 'Darwin' ]; then
     alias ls='ls -G'
 else
     alias ls='ls --color=auto'
 fi
-
-HISTFILE=~/.zsh_history
-HISTSIZE=10000
-SAVEHIST=10000
